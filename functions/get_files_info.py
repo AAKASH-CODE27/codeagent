88bb8751753c54055ee89fdad5b7a5ab5db5273c
AAKASH-CODE27 <aakashp27ece@gmail.com>
2026-06-20 21:12:19 +0530

    Done Gemini integration and tracking file tracking

---
+++ functions/get_files_info.py
@@ -0,0 +1,29 @@
+import os
+
+def get_files_info(working_directory: str, directory = "."):
+
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_directory = os.path.abspath(os.path.join(working_directory, directory))
+    # abs_directory = ""
+
+    # if directory is None:
+    #     abs_directory = os.path.abspath(working_directory)
+    # else:
+    #     # for multiple parameter passing -> to access sub directory
+    #     # print(get_files_info("calculator", "pkg"))
+    #     abs_directory = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not abs_directory.startswith(abs_working_dir):
+        return f'Error: "{directory}" is not in the working directory'
+    
+    final_response = ""
+
+    contents = os.listdir(abs_directory)
+
+    for content in contents:
+        content_path = os.path.join(abs_directory, content)
+
+        is_dir = os.path.isdir(content_path)
+        size = os.path.getsize(content_path)
+        final_response += f"-{content}: file_size={size} bytes, is_dir={is_dir}"
+    return final_response
---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai  #google sdk
+from google.genai import types
+from functions.get_files_info import get_files_info
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")  # api key extraction
+    # print("API key : ", api_key)
+
+    client = genai.Client(api_key = api_key)
+
+    if len(sys.argv) < 2:
+        print("Enter prompt completely!!")
+        sys.exit(1)
+
+    verbose_Flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_Flag = True
+    
+    prompt = sys.argv[1]
+   # print("Args", sys.argv) # sys argv for extracting sys arguements
+    
+    messages = [ types.Content(role="user", parts = [types.Part(text= prompt)])]  #A1
+
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = messages,
+    )
+
+    print(response.text)
+ 
+    if response is None or response.usage_metadata is None:
+        print("Invalid response")
+        return
+    
+    if verbose_Flag :
+
+        print(f"User Prompt : {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+# print(get_files_info("calculator"))
+# if __name__ == "__main__":  
+main()
+
+
+
+
+# """ triple quotes for multiline string
+#     otherwise need to use one double quotes and /n
+#     to enter for new line"""
+
+# sys.argv --> for extracting command line arguements
+
+# TO CHECK TOKEN USED
+#     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+#     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+# A1
+
+# User: What is Python?
+# Assistant: Python is a programming language.
+# User: Give an example.
+
+# messages = [
+#     {"role": "user", "content": "Hi"},
+#     {"role": "model", "content": "Hello"},
+#     {"role": "user", "content": "Explain Java"}
+# ]
+
+
+# verbose_flag
+#   if user asks for rate limit detail only we display them.
+#   they ask by --verbose after the prompt "what is 10 + 8" --verbose
+
