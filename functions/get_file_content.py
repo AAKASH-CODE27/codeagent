8cee74afab22fa59938e5600b0a5ac888661f0d7
AAKASH-CODE27 <aakashp27ece@gmail.com>
2026-06-21 18:07:29 +0530

    Write file impleamented

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -3,14 +3,14 @@ from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
 
-    abs_working_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_working_dir = os.path.abspath(working_directory)  # file path of directory
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))  # file path with text file lorem
  
-    if not abs_file_path.startswith(abs_working_dir):
+    if not abs_file_path.startswith(abs_working_dir): # esures valid file path is made
         return f'Error: "{file_path}" is not in the working directory'
     
-    if not os.path.isfile(abs_file_path):
-        return f'Error: "{fie_path}" is not a file'
+    if not os.path.isfile(abs_file_path): # ensures file path is valid here 
+        return f'Error: "{file_path}" is not a file'
     
     file_content_string = ""
     
@@ -25,3 +25,9 @@ def get_file_content(working_directory, file_path):
         return file_content_string
     except Exception as e:
         return f"Exception reading file: {e}"
+    
+
+
+# os -> The os module gives Python tools to work with files and folders.
+# os.path.abspath(folder)  -> gets file math 
+
--- tests.py
+++ tests.py
@@ -1,16 +1,23 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+
 
 def main():
     working_dir = "calculator"
-    root_contents = get_files_info(working_dir)
-    print(root_contents)
 
-    pkg_contents = get_files_info(working_dir,"pkg")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "/bin")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "../")
-    print(pkg_contents)
+    #get_file_content test 
+    # print(get_file_content(working_dir, "main.py"))
+    # print(get_file_content(working_dir, "pkg/calculator.py"))
+    # print(get_file_content(working_dir, "pkg/noteexists.py"))
+    # print(get_file_content(working_dir, "/bin/cat"))
+
+    # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    # print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    # print(write_file("calculator", "pkg2/temp.txt", "this should be allowed"))
+
+
+
 
 main()
 
